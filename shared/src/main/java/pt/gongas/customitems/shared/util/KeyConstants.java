b6d97688d674e7c67ec2bacbac2d51e8ab76e63c
Gonçalo <gongaslima2007@email.com>
2025-09-04 13:04:44 +0100

    v1.0.2

--- shared/src/main/java/pt/gongas/customitems/shared/util/KeyConstants.java
+++ shared/src/main/java/pt/gongas/customitems/shared/util/KeyConstants.java
@@ -2,6 +2,6 @@ package pt.gongas.customitems.shared.util;
 
 public class KeyConstants {
 
-    public static String ITEM_UUID = "turbocustomitems-random-uuid";
+    public static String ITEM_UUID = "turbocustomitems-random_uuid";
 
 }
